cfba2cfcf79fbf419f89cc8c657a63e80a629203
Romain-Constant <romain.constant59@gmail.com>
2023-10-10 18:44:24 +0200

    testing new mobile navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${prata.variable} w-screen overflow-x-hidden`}>
-        <header className="sticky top-0 z-50 ">
-          <Navbar />
-          <NewNavbarMobile />
-          <DoctolibStick />
-        </header>
+        <Navbar />
+        <NewNavbarMobile />
+        <DoctolibStick />
 
         {children}
         <Footer />
